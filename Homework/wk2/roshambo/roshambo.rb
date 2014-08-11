1628981716519ec1611221f48f0ac6073c13c261
Matthew Griffeth <matthewgriffeth@Matthews-MacBook-Pro.local>
2014-08-11 14:20:56 -0400

    ol' BLue EyEs

--- Homework/wk2/roshambo/roshambo.rb
+++ Homework/wk2/roshambo/roshambo.rb
@@ -1,14 +1,14 @@
 class Roshambo
 
-PLAYS = {
+$PLAYS = {
   :rock     => {:rock => :draw, :paper => :paper, :scissors => :rock},
   :paper    => {:rock => :paper, :paper => :draw, :scissors => :scissors},
   :scissors => {:rock => :rock, :paper => :scissors, :scissors => :draw}
 }
 
+attr_accessor = :plays
 
-
-  def initialize(plays2)
+  def initialize(plays)
   @plays = plays
   end
 
@@ -19,7 +19,8 @@ end
 
 
 class Roshambo_lizard_spock < Roshambo
-PLAYS2 = {
+
+plays2 = {
   :lizard => {:rock => :rock, :scissors => :scissors, :spock => :lizard,
             :lizard => :draw, :paper => :lizard},
   :spock  => {:lizard => :lizard, :paper => :paper, :spock => :draw,
@@ -28,18 +29,22 @@ PLAYS2 = {
   :paper  => {:lizard => :lizard, :spock => :paper},
   :scissors => {:lizard => :scissors, :spock => :spock}
 }
+    $PLAYS.merge!(plays2)
 
-  PLAYS.merge!(PLAYS2)
 
-  def initialize(plays2)
-    @plays2 = plays2
-  end
 
-  def play(p1, p2)
-    @plays2[p1][p2]
-  end
 end
 
-class Nerdshambo < Roshambo
+class Godmode < Roshambo
+plays3 ={
+  :god => {:rock => :god, :paper => :god, :scissors => :god, :lizard => :god,
+  :spock =>:god },
+  :rock => {:god => :god},
+  :paper => {:god => :god},
+  :scisors => {:god => :god},
+  :lizard => {:god => :god},
+  :spock => {:god =>:spock}
+}
+$PLAYS.merge!(plays3)
 
 end
